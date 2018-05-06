4d6fda7390ad961af62f959f8825c4440286a4bd
Jhonny Mata <solucionesmatajm@gmail.com>
2018-05-06 16:57:42 -0400

    añadido metodo getElement y forEach a la clase matrix

--- src/matrix.js
+++ src/matrix.js
@@ -19,6 +19,7 @@ function Matrix(data, width, height, dimension, options) {
     self.typeInstance = Array;
     self.length = 0; // tamaño del arreglo.
     self.dimension = 2;
+    assert(arguments.length > 0, "Es requerido un argumento");
     if (arguments.length == 1) {
         assert(typeof data == "object", "config debe ser un objeto.");
         setOptions(data);
@@ -113,5 +114,63 @@ function Matrix(data, width, height, dimension, options) {
                 throw new Error("El tipo de objeto no es valido...");
         }
     }
+    /**
+     * getIndex.
+     * Obtiene el indice del un arreglo unidimencional de la matriz.
+     * 
+     * @param {Array} arguments lista de argumentos del indice.
+     * @return {Number}
+     */
+    function getIndex(x, y, d) {
+        if (!d) {
+            d = (!isNaN(y)) ? 2 : 1;
+        }
+        if (isNaN(y)) {
+            y = 0;
+        }
+        assert(
+            typeof x == "number" && x >= 0 && x < self.width,
+            "el par x no es numero valido..."
+        );
+        assert(
+            typeof y == "number" && y >= 0 && y < self.height,
+            "el par y no es numero valido..."
+        );
+        assert(typeof d == "number" && d >= 0, "La dimension no es numero valido...");
+        assert(d <= self.dimension, "La dimensión excede el de la matriz...");
+        return Math.round(x + y * self.width) * (d - 1);
+    }    
+    /**
+     * getElement.
+     * Obtiene el valor del elemento actual.
+     * @param {Number} x Punto del plano cartesiano eje-x.
+     * @param {Number} y Punto del plano cartesiano eje-y.
+     * @param {Number} d Punto de la dimensión.
+     */
+    this.getElement = function(x, y, d) {
+        var index = getIndex(x, y, d);
+        return this.data[index];
+    };
+    /**
+     * forEach.
+     * Blucle para recorrer la matriz muldimensional.
+     */
+    this.forEach = function(callback) {
+        assert(callback instanceof Function, "callback debe ser una funcion.");
+        var width = self.width * (self.dimension - 1);
+        for(var index = 0, x = 0, y = 0, d = 0; index < this.length; index++) {
+            var element = this.data[index];
+            d++;
+            callback(element, x, y, d);
+            if (x >= width) {
+                x = 0;
+                y++;
+            }
+            if (d >= self.dimension) {
+                d = 0;
+            }
+            x++;
+        }
+    };
 }
 module.exports = Matrix;
